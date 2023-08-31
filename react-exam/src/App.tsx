8fb61f155b271ad7aebf6cbc9f05ef816a4c1f85
JunlinGong <913756523@qq.com>
2023-08-31 23:25:58 +0800

    feat: 配置@路径通配符

--- react-exam/src/App.tsx
+++ react-exam/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './page/login';
+import Login from '@/page/login';
 import './App.css';
 
 function App() {
