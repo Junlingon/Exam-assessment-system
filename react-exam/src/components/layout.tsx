875894cee86c98b6b0cc774076d4ca19a01aed1d
JunlinGong <913756523@qq.com>
2023-09-02 13:57:21 +0800

    feat: layout组件

---
+++ react-exam/src/components/layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+    return <div className="layout">
+        <div className="header_wrap"></div>
+        <div className="nav_wrap"></div>
+        <div className="outlet_wrap">
+            <Outlet />
+        </div>
+    </div>
+};
+
+export default Layout;
