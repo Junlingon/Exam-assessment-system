41da96371646c021865692b4b1d388a495418933
JunlinGong <913756523@qq.com>
2023-09-02 14:05:16 +0800

    feat: 配置scsss

--- react-exam/src/components/layout.tsx
+++ react-exam/src/components/layout.tsx
@@ -2,8 +2,12 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
     return <div className="layout">
-        <div className="header_wrap"></div>
-        <div className="nav_wrap"></div>
+        <div className="header_wrap">
+            head
+        </div>
+        <div className="nav_wrap">
+            nav
+        </div>
         <div className="outlet_wrap">
             <Outlet />
         </div>
