be0e8805324fa27c5731d2fc284b464f02fb969a
JunlinGong <913756523@qq.com>
2023-09-03 14:34:29 +0800

    feat: 引入antd

--- react-exam/src/components/layout.tsx
+++ react-exam/src/components/layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "./header";
+import Menu from "./menu";
 
 function Layout() {
     return <div className="layout">
         <div className="header_wrap">
-            head
+            <Header />
         </div>
         <div className="nav_wrap">
-            nav
+            <Menu />
         </div>
         <div className="outlet_wrap">
             <Outlet />
