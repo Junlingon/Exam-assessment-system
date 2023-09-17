199a97a40d4311d32c1f6ee3fe2dae6bddfe0c01
JunlinGong <913756523@qq.com>
2023-09-17 15:26:30 +0800

    feat: 完善person_info的联调逻辑

--- react-exam/src/page/person_info/index.tsx
+++ react-exam/src/page/person_info/index.tsx
@@ -1,29 +1,51 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { PlusOutlined } from '@ant-design/icons'
-import { DatePicker, Form, Input, Select, Upload, Button, message } from 'antd'
-import type { FormInstance } from 'antd/es/form'
-import type { UploadChangeParam } from 'antd/es/upload'
-import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface'
-import './index.scss'
-import IMG from './img/info.png'
-import { useNavigate } from 'react-router'
-import 'dayjs/locale/zh-cn'
-import locale from 'antd/es/date-picker/locale/zh_CN'
-import dayjs from 'dayjs'
+import React, { useEffect, useRef, useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { DatePicker, Form, Input, Select, Upload, Button, message } from 'antd';
+import type { FormInstance } from 'antd/es/form';
+import type { UploadChangeParam } from 'antd/es/upload';
+import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface';
+import './index.scss';
+import IMG from './img/info.png';
+import { useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
+import { upload_imgs } from '@/utils';
+import axios, { AxiosRes, ResData } from '@/utils/https';
+import { select_user_info, RoleState } from '@/store/slice/role';
+import 'dayjs/locale/zh-cn';
+import locale from 'antd/es/date-picker/locale/zh_CN';
+import dayjs from 'dayjs';
+import findRouter from '@/hooks/findRouter';
 
-const OPTIONS = ['大专', '本科', '硕士', '博士']
-let imgUrl = ''
+const OPTIONS = ['大专', '本科', '硕士', '博士'];
+let imgUrl = '';
 
 function PersonInfo() {
     const Navigate = useNavigate()
+    const { _id, has_person_info }: RoleState = useSelector(select_user_info)
     useEffect(() => {
+        if (has_person_info) {
+            init()
+        }
     }, [])
 
     const formRef = useRef<FormInstance>(null)
 
     const init = async () => {
+        const { data }: AxiosRes<ResData> = await axios.get('/api/user')
+        if (data.code === 0) {
+            form.setFieldsValue(data.data)
+            form.setFieldValue('graduation_time', dayjs(data.data.graduation_time))
+            setFileList([
+                {
+                    uid: data.data._id,
+                    name: 'backImg',
+                    status: 'done',
+                    url: `https://${data.data.avatar}`,
+                },
+            ])
+        }
     }
-    const has_person_info = false;
+
     // 图片列表
     const [fileList, setFileList] = useState<UploadFile[]>([])
     const imgTypeLimit = ['image/png', 'image/jpg', 'image/jpeg']
@@ -48,25 +70,52 @@ function PersonInfo() {
             return false
         },
         onChange: async (info: UploadChangeParam<UploadFile>) => {
+            console.log(info, '图片')
+            if (info.fileList.length > 0) {
+                setFileList(info.fileList)
+                const [imgURL] = (await upload_imgs(info.fileList).catch(() => {
+                    // 上传失败
+                    setFileList([])
+                })) as string[]
+                imgUrl = imgURL
+            }
 
-        }
-
-        /* if (info.file.status === 'uploading') {
-          return
-        }
-        if (info.file.status === 'done') {
-          // Get this url from response in real world.
-        } */
-        // },
+            /* if (info.file.status === 'uploading') {
+              return
+            }
+            if (info.file.status === 'done') {
+              // Get this url from response in real world.
+            } */
+        },
     }
 
     // 表单完成
     const onFinish = async (values: any) => {
-
+        // 1. 联调入参格式化formmat+接口调用
+        let params = {
+            ...values,
+            avatar: imgUrl,
+        }
+        const { data }: AxiosRes<ResData> = await axios.patch(`/api/user/${_id}`, params)
+        if (data.code === 0) {
+            if (!has_person_info) {
+                console.log(params, '参数')
+                // 2. 接口调用成功后
+                message.success('个人信息录入成功')
+                // 2.1 跳转考试页面
+                Navigate(findRouter('exam').path)
+            } else {
+                message.success('修改成功')
+            }
+        }
     }
 
     const onFinishFailed = () => {
-
+        if (!has_person_info) {
+            message.error('表单信息缺失')
+        } else {
+            message.error('修改失败')
+        }
     }
 
     return (
@@ -172,14 +221,14 @@ function PersonInfo() {
                         <div className='btn-container'>
                             {!has_person_info ? (
                                 <Button type='primary' htmlType='submit' className='btn goto_exam'>
-                                    保存信息
+                                    前往考试
                                 </Button>
                             ) : (
                                 <>
                                     <Button type='primary' htmlType='submit' className='btn'>
                                         确认修改
                                     </Button>
-                                    <Button type='default' className='btn gray-btn' onClick={() => { }}>
+                                    <Button type='default' className='btn gray-btn' onClick={() => Navigate(findRouter('exam').path)}>
                                         返回考试
                                     </Button>
                                 </>
