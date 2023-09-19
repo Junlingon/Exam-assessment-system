7ef50f398e3dbd399be8811368a89f710ab97a56
JunlinGong <913756523@qq.com>
2023-09-19 13:49:58 +0800

    feat: add '考试记录' page

--- react-exam/src/page/exam_history/index.tsx
+++ react-exam/src/page/exam_history/index.tsx
@@ -1,5 +1,32 @@
-function ExamHistory() {
-    return <h1>exam_history</h1>
+import React, { FC, useState, useEffect } from 'react';
+import { Table } from 'antd';
+import { tableDefaultData, tableColumns } from './interface';
+import PageList from '@/components/PageList';
+import styles from './index.module.scss';
+import { useAppDispatch, useAppSelector } from '@/store/index';
+import { get_exam_list, exam_list, exam_list_count } from '@/store/slice/exam';
+
+const ExamHistory: FC = () => {
+    const [list, setList] = useState(tableDefaultData)
+    const dispatch = useAppDispatch()
+    const [total, setTotal] = useState(0)
+    const exam = useAppSelector(exam_list)
+    const exam_count = useAppSelector(exam_list_count)
+    useEffect(() => {
+        dispatch(get_exam_list({}))
+    }, [])
+
+    /**
+     * 监视地址栏的page pageSize变化，发请求
+     */
+    return <div className={styles["exam-history"]}>
+        <div className='table-list-wrapper'>
+            <Table dataSource={exam} columns={tableColumns} pagination={false} />
+        </div>
+        <div className={styles["pagination-wrapper"]}>
+            <PageList total={exam_count} />
+        </div>
+    </div>;
 }
 
-export default ExamHistory;
+export default ExamHistory
--- react-exam/src/store/index.ts
+++ react-exam/src/store/index.ts
@@ -2,7 +2,8 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import subjectReducer from './slice/subject';
 import roleReducer from './slice/role';
-import menuReducer from './slice/menu'
+import menuReducer from './slice/menu';
+import examReducer from './slice/exam';
 
 // 类型定义
 export type AppDispatch = typeof store.dispatch
@@ -14,6 +15,7 @@ export const store = configureStore({
 		subject: subjectReducer,
 		role: roleReducer,
 		menu: menuReducer,
+		exam: examReducer
 	},
 })
 
