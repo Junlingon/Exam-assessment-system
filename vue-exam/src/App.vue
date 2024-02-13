911afaf03fa9f0a86419f45303189f3c700ff5f9
JunlinGong <913756523@qq.com>
2024-02-14 00:06:18 +0800

    feat: 项目初始化

--- vue-exam/src/App.vue
+++ vue-exam/src/App.vue
@@ -1,32 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import { onMounted, onBeforeMount } from 'vue'
+
+onBeforeMount(() => {
+
+})
+
+onMounted(async () => {
+
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="header_layout">
+    hello world
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <RouterView />
+  <div class="footer_layout">
+
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- vue-exam/src/main.ts
+++ vue-exam/src/main.ts
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+
+// import 'element-plus/dist/index.css'
+// 全局样式
+import './assets/main.css'
+import Colada, { PiniaColadaPlugin } from 'colada-plugin';
+
+const app = createApp(App)
+
+// 插件
+const pinia = createPinia()
+app.use(pinia)
+pinia.use(PiniaColadaPlugin);
+app.use(Colada);
+app.use(router)
+app.use(ElementPlus)
+
+app.mount('#app')
