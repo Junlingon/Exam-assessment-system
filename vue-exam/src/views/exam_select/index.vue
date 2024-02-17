0abed3e73ed4a31fc4c26886d489ce61d2a4358a
JunlinGong <913756523@qq.com>
2024-02-17 16:44:52 +0800

    feat: 完成考题选择

--- vue-exam/src/views/exam_select/index.vue
+++ vue-exam/src/views/exam_select/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted, onBeforeMount } from 'vue'
-
+import Item from './item.vue'
 
 onBeforeMount(() => {
 
@@ -12,9 +12,69 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
-    exam select
+  <div class="wrap">
+    <div class="one_wrap">
+      <div class="title">
+        React
+      </div>
+      <div class="two_wrap">
+        <Item name="测试数据" class="item" />
+        <Item name="测试数据" />
+        <Item name="测试数据" class="item" />
+        <Item name="测试数据" />
+        <Item name="测试数据" class="item" />
+        <Item name="测试数据" />
+        <Item name="测试数据" class="item" />
+        <Item name="测试数据" />
+        <Item name="测试数据" class="item" />
+        <Item name="测试数据" />
+        <Item name="测试数据" class="item" />
+        <Item name="测试数据" />
+
+      </div>
+    </div>
+
+    <div class="btn_wrap">
+      <el-button class="btn" size="large" type="primary">开始考试</el-button>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.wrap {
+  position: relative;
+  height: calc(100vh - 220px);
+
+  .title {
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
+
+  .two_wrap {
+    display: flex;
+    // flex-direction: row;
+    flex-flow: row wrap;
+  }
+
+  .item {
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+
+  .btn_wrap {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    border-top: 1px solid #ccc;
+    display: flex;
+    justify-content: center;
+
+    .btn {
+      // margin: 0 auto;
+      width: 200px;
+    }
+  }
+
+}</style>
---
+++ vue-exam/src/views/exam_select/item.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { onMounted, onBeforeMount } from 'vue'
+const emit = defineEmits(['click'])
+let props = defineProps({
+    active: Boolean,
+    disabled: Boolean,
+    name: String,
+    id: String
+})
+
+function item_click() {
+    if (!props.disabled) {
+        emit('click', props.id)
+    }
+}
+
+onBeforeMount(() => {
+
+})
+
+onMounted(async () => {
+
+})
+</script>
+
+<template>
+    <div @click="item_click" class="item" :class="{
+        active: props.active,
+        disabled: props.disabled
+    }">
+        {{ props.name }}
+        <el-icon v-if="props.active" class="icon"><Select /></el-icon>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.item {
+    width: 240px;
+    height: 40px;
+    background: #a6c8be;
+    border-radius: 6px;
+    text-align: center;
+    line-height: 40px;
+    position: relative;
+    cursor: pointer;
+
+    &.active {
+        border: 1px solid #1880FF;
+        color: #1880FF;
+        font-weight: bold;
+    }
+
+    &.disabled {
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+
+    .icon {
+        position: absolute;
+        right: 5px;
+        bottom: 5px;
+        color: #1880FF;
+    }
+}
+</style>
