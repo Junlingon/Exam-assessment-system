cafbb32962bde33e7a945ef735995b74709bb9d6
JunlinGong <913756523@qq.com>
2024-02-17 16:48:10 +0800

    feat: 添加科目

--- vue-exam/src/views/subject_add/index.vue
+++ vue-exam/src/views/subject_add/index.vue
@@ -1,20 +1,140 @@
 <script setup lang="ts">
 import { onMounted, onBeforeMount } from 'vue'
 
+import { ref } from 'vue'
 
-onBeforeMount(() => {
+const value = ref('')
 
-})
+const options = [
+  {
+    label: 'Popular cities',
+    options: [
+      {
+        value: 'Shanghai',
+        label: 'Shanghai',
+      },
+      {
+        value: 'Beijing',
+        label: 'Beijing',
+      },
+    ],
+  },
+  {
+    label: 'City name',
+    options: [
+      {
+        value: 'Chengdu',
+        label: 'Chengdu',
+      },
+      {
+        value: 'Shenzhen',
+        label: 'Shenzhen',
+      },
+      {
+        value: 'Guangzhou',
+        label: 'Guangzhou',
+      },
+      {
+        value: 'Dalian',
+        label: 'Dalian',
+      },
+    ],
+  },
+]
 
-onMounted(async () => {
-
-})
 </script>
 
 <template>
-  <div>
-    subject add
+  <div class="wrap">
+    <div class="top">
+      <p>xxxx课程</p>
+      <el-select v-modle="value" placeholder="Select">
+        <el-option-group v-for="group in options" :key="group.label" :label="group.label">
+          <el-option v-for="item in group.options" :key="item.value" :label="item.label" :value="item.value" />
+        </el-option-group>
+      </el-select>
+      <el-button class="btn" type="primary">新增题目</el-button>
+    </div>
+
+    <div class="content">
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.top {
+  display: flex;
+}
+</style>
+<script setup lang="ts">
+import { onMounted, onBeforeMount } from 'vue'
+
+import { ref } from 'vue'
+
+const value = ref('')
+
+const options = [
+  {
+    label: 'Popular cities',
+    options: [
+      {
+        value: 'Shanghai',
+        label: 'Shanghai',
+      },
+      {
+        value: 'Beijing',
+        label: 'Beijing',
+      },
+    ],
+  },
+  {
+    label: 'City name',
+    options: [
+      {
+        value: 'Chengdu',
+        label: 'Chengdu',
+      },
+      {
+        value: 'Shenzhen',
+        label: 'Shenzhen',
+      },
+      {
+        value: 'Guangzhou',
+        label: 'Guangzhou',
+      },
+      {
+        value: 'Dalian',
+        label: 'Dalian',
+      },
+    ],
+  },
+]
+
+</script>
+
+<template>
+  <div class="wrap">
+    <div class="top">
+      <p>xxxx课程</p>
+      <el-select v-modle="value" placeholder="Select">
+        <el-option-group v-for="group in options" :key="group.label" :label="group.label">
+          <el-option v-for="item in group.options" :key="item.value" :label="item.label" :value="item.value" />
+        </el-option-group>
+      </el-select>
+      <el-button class="btn" type="primary">新增题目</el-button>
+    </div>
+
+    <div class="content">
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.top {
+  display: flex;
+}
+</style>
