e792ed3f8b04ad90f72b1d574b5940f76587eb97
JunlinGong <913756523@qq.com>
2024-02-19 23:53:26 +0800

    feat: 课程管理ui界面

---
+++ vue-exam/src/views/subject_manage/add_modal.vue
@@ -0,0 +1,63 @@
+<template>
+    <el-button @click="add_click" class="btn" type="primary">新增课程</el-button>
+
+    <el-dialog v-model="dialogVisible" title="新增二级课程类目" width="30%" :before-close="handleClose">
+        <el-form :model="form" label-width="120px">
+            <el-form-item label="课程类别">
+                <el-select v-model="form.one_key" placeholder="please select your zone">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="课程名称">
+                <el-select v-model="form.two_name" placeholder="please select your zone">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                </el-select>
+            </el-form-item>
+        </el-form>
+
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="dialogVisible = false">
+                    Confirm
+                </el-button>
+            </span>
+        </template>
+    </el-dialog>
+</template>
+  
+<script lang="ts" setup>
+import { ref, reactive } from 'vue';
+
+let dialogVisible = ref(false)
+
+let form = reactive({
+    one_key: '',
+    two_name: ''
+})
+
+const add_click = () => {
+    dialogVisible.value = true;
+}
+
+const handleClose = (done: () => void) => {
+    dialogVisible.value = false;
+    // ElMessageBox.confirm('Are you sure to close this dialog?')
+    //   .then(() => {
+    //     done()
+    //   })
+    //   .catch(() => {
+    //     // catch error
+    //   })
+}
+</script>
+<style scoped>  .btn {
+      margin-bottom: 20px;
+  }
+
+  .dialog-footer button:first-child {
+      margin-right: 10px;
+  }
+</style>
