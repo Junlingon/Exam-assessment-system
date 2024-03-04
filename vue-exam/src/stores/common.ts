5c6bf9eae8460944e42b23e673c0d1c02c5e5299
JunlinGong <913756523@qq.com>
2024-03-04 22:56:20 +0800

    feat: 工具函数优化

--- vue-exam/src/stores/common.ts
+++ vue-exam/src/stores/common.ts
@@ -1,18 +1,17 @@
 import { reactive } from 'vue';
 import { defineStore } from 'pinia'
-import type { MenuData, UserInfo } from '../utils/request';
+import type { MenuData } from '../utils/request';
 
 export const useCommonStore = defineStore('common', {
   state: () => {
-    const userinfo: UserInfo = {
-      created: null,
+    const userinfo = {
       name: '',            // 学生花名
       vChat: '',          // 微信名字
       phone: '',          // 手机
       avatar: '',        // 头像
       graduation_time: '',    // 毕业时间
       money: 0,         // 现在薪资
-      role: 'student',        // 角色
+      role: '',        // 角色
       _id: '',
       has_person_info: false,  // 是否填写个人信息
       topic_role: [],        // 课程权限列表
